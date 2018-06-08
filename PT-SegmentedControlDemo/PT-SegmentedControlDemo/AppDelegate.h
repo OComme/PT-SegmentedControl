630769d4fc7b7c5c54c78f632a9b07a4f6286074
OComme <a1653913692@icloud.com>
2018-06-08 11:19:54 +0800

    [init]

---
+++ PT-SegmentedControlDemo/PT-SegmentedControlDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PT-SegmentedControlDemo
+//
+//  Created by BlanBok on 2018/6/8.
+//  Copyright © 2018年 OComme. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PT-SegmentedControlDemo/PT-SegmentedControlDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PT-SegmentedControlDemo
+//
+//  Created by BlanBok on 2018/6/8.
+//  Copyright © 2018年 OComme. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
